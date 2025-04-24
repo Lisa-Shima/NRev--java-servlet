5966f807edda1cf04ab6063445cdf8ba814de97e
Lisa-Shima <shimalisa2@gmail.com>
2025-04-24 18:24:00 +0200

    Committing - Servlet initialization

---
+++ src/main/java/com/nrev/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.nrev.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException{
+        System.out.println("HelloServlet initialized");
+    }
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        res.getWriter().println("<h1>Hello Servlets!</h1>");
+        System.out.println("doGet method called");
+    }
+
+    @Override
+    public void destroy(){
+        System.out.println("HelloServlet Destroyed");
+    }
+}

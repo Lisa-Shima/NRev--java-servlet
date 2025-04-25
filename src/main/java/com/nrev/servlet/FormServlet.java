183cdd1933e5ae06198058f2ebec97843a5178cf
Lisa-Shima <shimalisa2@gmail.com>
2025-04-25 17:13:47 +0200

    Committing - Added Session Management

--- src/main/java/com/nrev/servlet/FormServlet.java
+++ src/main/java/com/nrev/servlet/FormServlet.java
@@ -4,18 +4,27 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
 public class FormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//        Retrieving data from the form
         String name = req.getParameter("name");
         String email = req.getParameter("email");
+//        Session management
+        HttpSession session = req.getSession();
+        session.setAttribute("name", name);
+        session.setAttribute("email", email);
 
-        res.setContentType("text/html");
-        res.getWriter().println("<h1>Form successfully submitted</h1>");
-        res.getWriter().println("<p>Name: " + name + "</p>");
-        res.getWriter().println("<p>Email: " + email + "</p>");
+//        res.setContentType("text/html");
+//        res.getWriter().println("<h1>Form successfully submitted</h1>");
+//        res.getWriter().println("<p>Name: " + name + "</p>");
+//        res.getWriter().println("<p>Email: " + email + "</p>");
+
+//        Redirect to another page
+        res.sendRedirect("greet");
     }
 }
---
+++ src/main/java/com/nrev/servlet/GreetServlet.java
@@ -0,0 +1,24 @@
+package com.nrev.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class GreetServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session != null){
+            String name = (String) session.getAttribute("name");
+            res.setContentType("text/html");
+            res.getWriter().println("<h1>Welcome, " + name + "</h1>");
+        }
+        else{
+            res.getWriter().println("<p>No session found!</p>");
+        }
+    }
+}

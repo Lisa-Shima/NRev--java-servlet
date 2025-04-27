d28442cb6d27557500fc7acbfa7c2aca5fa5f19a
Lisa-Shima <shimalisa2@gmail.com>
2025-04-27 23:22:33 +0200

    Committing - Added Custom Error Pages

---
+++ src/main/java/com/nrev/TestServlet.java
@@ -0,0 +1,15 @@
+package com.nrev;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        throw new RuntimeException("Test 500 Error");
+    }
+}

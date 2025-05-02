a8c1f1766c53600909e84c38f5da7118055805b0
Lisa-Shima <shimalisa2@gmail.com>
2025-05-02 17:19:57 +0200

    Committing - Integrated Database; Added User to DB

--- src/main/java/com/nrev/util/DatabaseUtil.java
+++ src/main/java/com/nrev/util/DatabaseUtil.java
@@ -1,2 +1,24 @@
-package com.nrev.util;public class DatabaseUtil {
+package com.nrev.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static final String URL = "jdbc:postgresql://localhost:5432/nrevservletsdb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "p2e3r5l";
+
+    static{
+        try{
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e){
+            throw new RuntimeException("PostgreSQL JDBC Driver not found!" + e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
